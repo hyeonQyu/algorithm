07d3c714ea29fbc059b0e4f2d8cc1012fd291f2e
hyeonQyu <dhk0561@naver.com>
2019-07-04 21:44:09 +0900

    <programmers> level1) 문자열 내 p와 y의 개수

---
+++ programmers/level1/StringPY.java
@@ -0,0 +1,24 @@
+package level1;
+
+public class StringPY {
+
+	public static boolean solution(String s) {
+		boolean answer = true;
+		int countP = 0;
+		int countY = 0;
+		
+		s = s.toLowerCase();
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == 'p')
+				countP++;
+			else if(s.charAt(i) == 'y')
+				countY++;
+		}
+		
+		if(countP != countY)
+			answer = false;
+		
+		return answer;
+	}
+
+}
