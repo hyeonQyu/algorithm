155a9930d2ea408ede9ab1230b6e97319a68fffb
hyeonQyu <dhk0561@naver.com>
2019-07-05 20:26:55 +0900

    <programmers> level1) 서울에서 김서방 찾기

---
+++ programmers/level1/MrKim.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class MrKim {
+	
+	public static String solution(String[] seoul) {
+		for(int i = 0; i < seoul.length; i++) {
+			if(seoul[i].equals("Kim"))
+				return "김서방은 " + i + "에 있다";
+		}
+		return null;
+	}
+
+}
