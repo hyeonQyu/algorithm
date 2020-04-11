23860dd321a108b30678d0513ec7354e29304113
hyeonQyu <44297538+hyeonQyu@users.noreply.github.com>
2020-04-12 01:20:45 +0900

    Update LeavingCompany.java
    14501) 퇴사 [동적계획법, 브루트포스]

--- BaekJoon/bruteForce/LeavingCompany.java
+++ BaekJoon/bruteForce/LeavingCompany.java
@@ -3,8 +3,8 @@ package bruteForce;
 import java.io.*;
 
 public class LeavingCompany {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int n = Integer.parseInt(br.readLine());
