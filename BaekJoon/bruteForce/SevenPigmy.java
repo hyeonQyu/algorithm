dc8ce7fdd1fd9183f801bfc38c51e70d2cb566f5
hyeonQyu <dhk0561@naver.com>
2020-04-11 20:46:52 +0900

    2309) 일곱 난쟁이 [브루트포스]

--- BaekJoon/bruteForce/SevenPigmy.java
+++ BaekJoon/bruteForce/SevenPigmy.java
@@ -1,55 +1,52 @@
 package bruteForce;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class SevenPigmy {
-	
-	static final int SUM_OF_HEIGHT = 100;
-	static final int NUM_OF_PIGMY = 7;
-	static int[] height = new int[9];
-	static boolean[] isPigmy = new boolean[9];
-	
-	public static void addToSumOfHeight(int index, int sum, int total, int count) {
-		if(isPrommising(index, sum, total, count)) {
-			if(sum == SUM_OF_HEIGHT && count == NUM_OF_PIGMY) {
-				for(int i = 0; i < height.length; i++) {
-					if(isPigmy[i]) {
-						System.out.println(height[i]);
-					}
-				}
-			}
-			else {
-				try {
-					isPigmy[index + 1] = true;
-					addToSumOfHeight(index + 1, sum + height[index+1], total - height[index+1], count + 1);
-					isPigmy[index + 1] = false;
-					addToSumOfHeight(index + 1, sum, total - height[index+1], count);
-				}
-				catch(ArrayIndexOutOfBoundsException e) {
-					System.exit(0);
-				}
-			}
-		}
-	}
-	
-	public static boolean isPrommising(int index, int sum, int total, int count) {
-		return (sum + total >= SUM_OF_HEIGHT) && (sum == SUM_OF_HEIGHT || sum + height[index+1] <= SUM_OF_HEIGHT) && (count <= NUM_OF_PIGMY);
-	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		
-		int total = 0;
-		for(int i = 0; i < height.length; i++) {
-			height[i] = scanner.nextInt();
-			total += height[i];
-		}
-		Arrays.sort(height);
-		
-		addToSumOfHeight(-1, 0, total, 0);
-	
-		scanner.close();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int[] pigmies = new int[9];
+        int sum = 0;
+        for(int i = 0; i < 9; i++){
+            pigmies[i] = Integer.parseInt(br.readLine());
+            sum += pigmies[i];
+        }
+        
+        boolean isFind = false;
+        for(int i = 0; i < 9; i++){
+            int p1 = pigmies[i];
+            for(int j = 0; j < 9; j++){
+                if(i == j)
+                    continue;
+                int p2 = pigmies[j];
+                
+                if(sum - (p1 + p2) == 100){
+                    pigmies[i] = 0;
+                    pigmies[j] = 0;
+                    isFind = true;
+                    break;
+                }
+            }
+            if(isFind)
+                break;
+        }
+        
+        Arrays.sort(pigmies);
+        
+        for(int i = 2; i < 9; i++){
+            bw.write(pigmies[i] + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
 	}
 
 }
