647cad24ce6ebef2309dc3700ba3314bc35c8d3f
hyeonQyu <dhk0561@naver.com>
2020-04-11 16:45:19 +0900

    2579) 계단 오르기 [동적계획법]

--- BaekJoon/dynamicProgramming/ClimbingStair.java
+++ BaekJoon/dynamicProgramming/ClimbingStair.java
@@ -1,42 +1,33 @@
 package dynamicProgramming;
-import java.util.Scanner;
 
-class Stair {
-	int point;
-	int sumOfPoints;
-	
-	Stair(int point) {
-		this.point = point;
-		sumOfPoints = 0;
-	}
-}
+import java.util.Scanner;
 
 public class ClimbingStair {
 	
-	static int getMax(int sum1, int sum2) {
-		return sum1 > sum2 ? sum1 : sum2;
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		Stair[] stairs = new Stair[n];
-		
-		for(int i = 0; i < n; i++)
-			stairs[i] = new Stair(scanner.nextInt());
-		
-		stairs[0].sumOfPoints = stairs[0].point;
-		stairs[1].sumOfPoints = stairs[0].sumOfPoints + stairs[1].point;
-		stairs[2].sumOfPoints = getMax(stairs[0].point + stairs[2].point, stairs[1].point + stairs[2].point);
-		for(int i = 3; i < n; i++)
-			// ÀüÀüÄ­À» ¹â°í ÇöÀçÄ­À» ¹â´Â °æ¿ì -> ÀüÀüÄ­±îÁöÀÇ ÃÖ´ë°ª + ÇöÀçÄ­
-			// ÀüÄ­À» ¹â°í ÇöÀçÄ­À» ¹â´Â °æ¿ì -> ÀüÀüÀüÄ­±îÁöÀÇ ÃÖ´ë°ª + ÀüÄ­ + ÇöÀçÄ­
-			stairs[i].sumOfPoints = getMax(stairs[i-2].sumOfPoints + stairs[i].point, stairs[i-3].sumOfPoints + stairs[i-1].point + stairs[i].point);
-		
-		System.out.println(stairs[n-1].sumOfPoints);
-		
-		scanner.close();
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] stairs = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            stairs[i] = sc.nextInt();
+        }
+        
+        if(n == 1){
+            System.out.print(stairs[0]);
+            return;
+        }
+        
+        int[][] d = new int[n][2];
+        d[0][0] = d[0][1] = stairs[0];
+        d[1][0] = stairs[1];
+        d[1][1] = stairs[0] + stairs[1];
+        for(int i = 2; i < n; i++){
+            d[i][0] = Math.max(d[i - 2][0], d[i - 2][1]) + stairs[i];
+            d[i][1] = d[i - 1][0] + stairs[i];
+        }
+        
+        System.out.println(Math.max(d[n - 1][0], d[n - 1][1]));
 	}
 
 }
