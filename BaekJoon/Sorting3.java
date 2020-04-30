6a9200f7206907e73e7120eb2c2bd8c71c6ac5f7
hyeonQyu <dhk0561@naver.com>
2020-04-30 16:36:38 +0900

    10989) 수 정렬하기 3 [정렬]

---
+++ BaekJoon/Sorting3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Sorting3 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder("");
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(br.readLine());
+            arr[num]++;
+        }
+        for(int i = 1; i < 10001; i++){
+            for(int j = 0; j < arr[i]; j++){
+                answer.append(Integer.toString(i) + "\n");
+            }
+        }
+        System.out.println(answer);
+    }
+
+}
