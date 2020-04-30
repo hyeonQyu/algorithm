b2cc62f048d8d326de9abd118adb2359c5ef9210
hyeonQyu <dhk0561@naver.com>
2020-04-30 16:30:55 +0900

    11726) 2xn 타일링 [동적계획법]

---
+++ BaekJoon/Nx2Tiling.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Nx2Tiling {
+
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] d = new int[n];
+        d[0] = 1;
+        if(n < 2){
+            System.out.println(d[0]);
+            return;
+        }
+        d[1] = 2;
+        
+        for(int i = 2; i < n; i++){
+        	d[i] = (d[i - 1] + d[i - 2]) % 10007;
+        }
+        
+        System.out.println(d[n - 1]);
+    }
+	
+}
