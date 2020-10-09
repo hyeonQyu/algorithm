5f07e16f56083dbb70fc71ac529cde1ccc714a2b
hyeonQyu <dhk0561@naver.com>
2020-10-09 18:27:55 +0900

    <programmers> 2019 Winter Intership) 튜플 [해쉬셋, Comparator]

--- programmers/kakao/Tuple.java
+++ programmers/kakao/Tuple.java
@@ -9,44 +9,41 @@ public class Tuple {
 	}
 	
 	 public int[] solution(String s) {
-		 public int[] solution(String s) {
-		        s = s.substring(2, s.length() - 2);
-		        String[] arr = s.split("\\}\\,\\{");
-		        // 길이순으로 정렬
-		        Arrays.sort(arr, new Comparator<String>(){
-		            public int compare(String a, String b){
-		                return Integer.compare(a.length(), b.length());
-		            }
-		        });
-		        
-		        int length = arr.length;
-		        String[][] tuples = new String[length][];
-		        int[] answer = new int[length];
-		        boolean[] check = new boolean[100001];
-		        for(int i = 0; i < length; i++){
-		            tuples[i] = arr[i].split(",");
-		            int tLength = tuples[i].length;
-		            
-		            HashSet<Integer> set = new HashSet<>();
-		            for(int j = 0; j < tLength; j++){
-		                int num = Integer.parseInt(tuples[i][j]);
-		                if(!check[num]){
-		                    answer[i] = num;
-		                    check[num] = true;
-		                    break;
-		                }
-		                else{
-		                    if(set.contains(num)){
-		                        answer[i] = num;
-		                        break;
-		                    }
-		                    set.add(num);
-		                }
-		            }
-		        }
-		        
-		        return answer;
-		    }
+		 s = s.substring(2, s.length() - 2);
+	        String[] arr = s.split("\\}\\,\\{");
+	        // 길이순으로 정렬
+	        Arrays.sort(arr, new Comparator<String>(){
+	            public int compare(String a, String b){
+	                return Integer.compare(a.length(), b.length());
+	            }
+	        });
+	        
+	        int length = arr.length;
+	        String[][] tuples = new String[length][];
+	        int[] answer = new int[length];
+	        boolean[] check = new boolean[100001];
+	        for(int i = 0; i < length; i++){
+	            tuples[i] = arr[i].split(",");
+	            int tLength = tuples[i].length;
+	            
+	            HashSet<Integer> set = new HashSet<>();
+	            for(int j = 0; j < tLength; j++){
+	                int num = Integer.parseInt(tuples[i][j]);
+	                if(!check[num]){
+	                    answer[i] = num;
+	                    check[num] = true;
+	                    break;
+	                }
+	                else{
+	                    if(set.contains(num)){
+	                        answer[i] = num;
+	                        break;
+	                    }
+	                    set.add(num);
+	                }
+	            }
+	        }
+	        
+	        return answer;
 	    }
-
 }
